9008cc716055bdd6b0990abebd75ad213f6c910a
Daniel <41750841+vely44@users.noreply.github.com>
2023-07-18 14:35:22 +0200

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,26 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
+import { Auth } from "aws-amplify"; // Import Amplify's Auth module
 
 const Login: NextPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      // Sign in with Amplify Auth
+      await Auth.signIn(username, password);
+      setMessage("Login successful!");
+    } catch (error) {
+      setMessage("Login failed: " + error.message);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -15,16 +33,22 @@ const Login: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Login to <span className="text-[#e74c3c]">Z Gen App</span>
           </h1>
-          <form className="flex flex-col gap-4 max-w-md w-full">
+          <form className="flex flex-col gap-4 max-w-md w-full" onSubmit={handleLogin}>
             <input
               type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               className="rounded-md px-4 py-2 bg-[#2e2e2e] text-white placeholder-[#8c8c8c] focus:outline-none"
               placeholder="Username"
+              required
             />
             <input
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="rounded-md px-4 py-2 bg-[#2e2e2e] text-white placeholder-[#8c8c8c] focus:outline-none"
               placeholder="Password"
+              required
             />
             <button
               type="submit"
@@ -33,6 +57,7 @@ const Login: NextPage = () => {
               Login
             </button>
           </form>
+          {message && <p className="text-white">{message}</p>}
           <div className="mt-4">
             <Link href="/">
               <a className="text-[#e74c3c] font-semibold text-lg hover:underline">
